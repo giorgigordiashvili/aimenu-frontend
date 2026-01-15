4930bf2e5945a2b462346340ba443e1abb9624dc
GioLa05 <giorgich535@gmail.com>
2026-01-15 12:53:52 +0400

    prettier error fix

--- src/icons/Location.tsx
+++ src/icons/Location.tsx
@@ -1,23 +1,20 @@
-import * as React from "react"
+import * as React from 'react';
 const LocationIcon = () => (
-  <svg
-    xmlns="http://www.w3.org/2000/svg"
-    fill="none"
-  >
+  <svg xmlns='http://www.w3.org/2000/svg' fill='none'>
     <path
-      stroke="#90A1B9"
-      strokeLinecap="round"
-      strokeLinejoin="round"
+      stroke='#90A1B9'
+      strokeLinecap='round'
+      strokeLinejoin='round'
       strokeWidth={1.333}
-      d="M13.333 6.666c0 3.329-3.692 6.796-4.932 7.866a.667.667 0 0 1-.802 0c-1.24-1.07-4.932-4.537-4.932-7.866a5.333 5.333 0 1 1 10.666 0Z"
+      d='M13.333 6.666c0 3.329-3.692 6.796-4.932 7.866a.667.667 0 0 1-.802 0c-1.24-1.07-4.932-4.537-4.932-7.866a5.333 5.333 0 1 1 10.666 0Z'
     />
     <path
-      stroke="#90A1B9"
-      strokeLinecap="round"
-      strokeLinejoin="round"
+      stroke='#90A1B9'
+      strokeLinecap='round'
+      strokeLinejoin='round'
       strokeWidth={1.333}
-      d="M8 8.667a2 2 0 1 0 0-4 2 2 0 0 0 0 4Z"
+      d='M8 8.667a2 2 0 1 0 0-4 2 2 0 0 0 0 4Z'
     />
   </svg>
-)
-export default LocationIcon 
+);
+export default LocationIcon;
