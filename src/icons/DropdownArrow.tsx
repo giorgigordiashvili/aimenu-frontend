f052da194fc70c355d4317605f5d3b0fa789418a
GioLa05 <giorgich535@gmail.com>
2026-01-22 18:44:51 +0400

    Dropdown.tsx done

---
+++ src/icons/DropdownArrow.tsx
@@ -0,0 +1,14 @@
+import * as React from 'react';
+const DropdownArrow = () => (
+  <svg xmlns='http://www.w3.org/2000/svg' width={16} height={16} fill='none'>
+    <path
+      stroke='#717182'
+      strokeLinecap='round'
+      strokeLinejoin='round'
+      strokeWidth={1.333}
+      d='m4 6 4 4 4-4'
+      opacity={0.5}
+    />
+  </svg>
+);
+export default DropdownArrow;
