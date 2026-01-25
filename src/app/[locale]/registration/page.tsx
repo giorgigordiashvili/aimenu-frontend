aab0dc3ec3905fed1d8eec9e7cbc1682e8e51999
GioLa05 <giorgich535@gmail.com>
2026-01-25 16:55:13 +0400

    RadioButton done

--- src/app/[locale]/registration/page.tsx
+++ src/app/[locale]/registration/page.tsx
@@ -2,14 +2,17 @@
 
 import React from 'react';
 
-import Checkbox from '@/components/Checkbox/Checkbox';
+import RadioButton from '@/components/RadioButton/RadioButton';
 
 function Page() {
   return (
     <>
-      <Checkbox label='Accept Terms and Conditions' />
-      <Checkbox label='Accept Privacy Policy' />
-      <Checkbox label='Accept Marketing Communications' disabled />
+      <RadioButton label='Option 1' name='example' />
+      <RadioButton label='Option 2' name='example' disabled />
+      <RadioButton label='Option 3' name='example' />
+      <RadioButton label='Option 4' name='sxva' />
+      <RadioButton label='Option 5' name='sxva' disabled />
+      <RadioButton label='Option 6' name='sxva' />
     </>
   );
 }
