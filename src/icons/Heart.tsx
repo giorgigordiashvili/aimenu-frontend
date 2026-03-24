e9fbf87d2cc909c7fd317fd5109f61245a6e6d50
GioLa05 <giorgich535@gmail.com>
2026-03-24 19:28:37 +0000

    fix: Heart icon viewBox expanded to contain stroke, remove overflow visible

--- src/icons/Heart.tsx
+++ src/icons/Heart.tsx
@@ -1,7 +1,7 @@
 import * as React from 'react';
 
 const HeartIcon = (props: React.SVGProps<SVGSVGElement>) => (
-  <svg xmlns='http://www.w3.org/2000/svg' width={16} height={16} fill='none' overflow='visible' {...props}>
+  <svg xmlns='http://www.w3.org/2000/svg' width={16} height={16} viewBox='-1 -1 18 18' fill='none' {...props}>
     <path
       stroke='currentColor'
       strokeLinecap='round'
