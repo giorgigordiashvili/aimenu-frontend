be42995bbe94efb83cf01d879353ccc47e5785ba
GioLa05 <giorgich535@gmail.com>
2026-03-24 19:25:15 +0000

    fix: Heart icon overflow visible to prevent stroke clipping

--- src/icons/Heart.tsx
+++ src/icons/Heart.tsx
@@ -1,7 +1,7 @@
 import * as React from 'react';
 
 const HeartIcon = (props: React.SVGProps<SVGSVGElement>) => (
-  <svg xmlns='http://www.w3.org/2000/svg' width={16} height={16} fill='none' {...props}>
+  <svg xmlns='http://www.w3.org/2000/svg' width={16} height={16} fill='none' overflow='visible' {...props}>
     <path
       stroke='currentColor'
       strokeLinecap='round'
