6c81a5da4a2af12f0e01121680ba3c39404fd6a3
GioLa05 <giorgich535@gmail.com>
2026-03-24 19:07:39 +0000

    fix: use Heart.tsx icon for favorite button

--- src/components/RestaurantCardPrimary/DefaultVariant.tsx
+++ src/components/RestaurantCardPrimary/DefaultVariant.tsx
@@ -4,6 +4,7 @@ import { styled } from '@pigment-css/react';
 import { useRouter } from 'next/navigation';
 
 import MainButton from '@/components/MainButton/MainButton';
+import Heart from '@/icons/Heart';
 import LocationIcon from '@/icons/Location';
 import Star from '@/icons/Star';
 import {
@@ -224,13 +225,9 @@ export default function DefaultVariant({
             style={{ color: isFavorited ? redBrand : slate400 }}
           >
             {isFavorited ? (
-              <svg width={14} height={14} viewBox="0 0 24 24" fill="currentColor">
-                <path d="M12 21.593c-5.63-5.539-11-10.297-11-14.402 0-3.791 3.068-5.191 5.281-5.191 1.312 0 4.151.501 5.719 4.457 1.59-3.968 4.464-4.447 5.726-4.447 2.54 0 5.274 1.621 5.274 5.181 0 4.069-5.136 8.625-11 14.402z" />
-              </svg>
+              <Heart width={14} height={14} fill='currentColor' stroke='currentColor' />
             ) : (
-              <svg width={14} height={14} viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth={2}>
-                <path d="M12 21.593c-5.63-5.539-11-10.297-11-14.402 0-3.791 3.068-5.191 5.281-5.191 1.312 0 4.151.501 5.719 4.457 1.59-3.968 4.464-4.447 5.726-4.447 2.54 0 5.274 1.621 5.274 5.181 0 4.069-5.136 8.625-11 14.402z" />
-              </svg>
+              <Heart width={14} height={14} />
             )}
           </FavoriteButton>
         </FavoriteGroup>
--- src/icons/Heart.tsx
+++ src/icons/Heart.tsx
@@ -1,8 +1,9 @@
 import * as React from 'react';
-const HeartIcon = () => (
-  <svg xmlns='http://www.w3.org/2000/svg' width={16} height={16} fill='none'>
+
+const HeartIcon = (props: React.SVGProps<SVGSVGElement>) => (
+  <svg xmlns='http://www.w3.org/2000/svg' width={16} height={16} fill='none' {...props}>
     <path
-      stroke='#fff'
+      stroke='currentColor'
       strokeLinecap='round'
       strokeLinejoin='round'
       strokeWidth={1.333}
